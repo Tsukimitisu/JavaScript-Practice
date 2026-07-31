06ff134637c25ff4772e094a0dc0b62ea12fd08f
James Andrei Revilla <revillajamesandrei4@gmail.com>
2026-07-31 23:38:52 +0800

    feat: make todo list interactive

--- Array-And-Loops/todo-list.js
+++ Array-And-Loops/todo-list.js
@@ -1,13 +1,40 @@
 const todoList = [];
+const formElement = document.querySelector('.js-todo-form');
+const inputElement = document.querySelector('.js-input');
+const listElement = document.querySelector('.js-todo-list');
 
-function addTodo(){
-    const inputEl = document.querySelector('.js-input')
-    const name = inputEl.value
-    
+function renderTodoList() {
+    listElement.replaceChildren();
+
+    todoList.forEach((todo, index) => {
+        const itemElement = document.createElement('li');
+        const removeButton = document.createElement('button');
+
+        itemElement.append(todo, ' ');
+        removeButton.type = 'button';
+        removeButton.textContent = 'Remove';
+        removeButton.setAttribute('aria-label', `Remove ${todo}`);
+        removeButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+            renderTodoList();
+        });
+
+        itemElement.append(removeButton);
+        listElement.append(itemElement);
+    });
+}
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = inputElement.value.trim();
+
+    if (!name) {
+        return;
+    }
 
     todoList.push(name);
-
-    console.log(todoList)
-
-    inputEl.value = ''
-}
+    renderTodoList();
+    formElement.reset();
+    inputElement.focus();
+});
