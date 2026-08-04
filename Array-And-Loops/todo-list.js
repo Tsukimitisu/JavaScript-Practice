0e384347af5b5581eb9fe8db741459785330b51e
James Andrei Revilla <revillajamesandrei4@gmail.com>
2026-08-04 22:26:54 +0800

    feat: persist todo list across reloads

--- Array-And-Loops/todo-list.js
+++ Array-And-Loops/todo-list.js
@@ -1,7 +1,23 @@
-const todoList = [];
+const storageKey = 'todo-list';
+const todoList = loadTodoList();
 const formElement = document.querySelector('.js-todo-form');
 const inputElement = document.querySelector('.js-input');
 const listElement = document.querySelector('.js-todo-list');
+
+function loadTodoList() {
+    try {
+        const savedTodos = JSON.parse(localStorage.getItem(storageKey));
+        return Array.isArray(savedTodos)
+            ? savedTodos.filter((todo) => typeof todo === 'string')
+            : [];
+    } catch {
+        return [];
+    }
+}
+
+function saveTodoList() {
+    localStorage.setItem(storageKey, JSON.stringify(todoList));
+}
 
 function renderTodoList() {
     listElement.replaceChildren();
@@ -16,6 +32,7 @@ function renderTodoList() {
         removeButton.setAttribute('aria-label', `Remove ${todo}`);
         removeButton.addEventListener('click', () => {
             todoList.splice(index, 1);
+            saveTodoList();
             renderTodoList();
         });
 
@@ -34,7 +51,10 @@ formElement.addEventListener('submit', (event) => {
     }
 
     todoList.push(name);
+    saveTodoList();
     renderTodoList();
     formElement.reset();
     inputElement.focus();
 });
+
+renderTodoList();
